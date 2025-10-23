9b590ca707b1bf0c490e0dc1268054322d116eae
Serhii Petrychenko <serhiipetrychenko.main@gmail.com>
2025-10-24 00:45:08 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from random import choice
+from string import ascii_letters, digits, punctuation
+
+length = int(input("Enter password length: "))
+number_of_passwords = int(input("Enter the number of passwords: "))
+special_symbols = input("Special characters required? (+/-): ")
+mode = input("Display passwords on screen (-) or save to file (+)? (+/-): ")
+
+if mode == "+":
+    print("Passwords saved to file passwords.txt")
+    with open("passwords.txt", "w", encoding="utf-8") as file:
+        file.write("Generated passwords:\n")
+
+for _ in range(number_of_passwords):
+    if special_symbols == "+":
+        characters = ascii_letters + digits + punctuation
+    else:
+        characters = ascii_letters + digits
+    password = ''.join(choice(characters) for _ in range(length))
+    if mode == "+":
+        with open("passwords.txt", "a", encoding="utf-8") as file:
+            file.write(f"{password}\n")
+    else:
+         print(password)
+
+input("Press Enter to exit...")
